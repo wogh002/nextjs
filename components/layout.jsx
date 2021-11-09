26201b2df957f1f5bec4a23cae7c0a2a737e595a
wogh002 <ekekekem159@gmail.com>
2021-11-09 21:21:40 +0900

    test

---
+++ components/layout.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Head from 'next/head'
+import styles from './layout.module.css';
+import Image from 'next/image';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'jaeho~';
+export const siteTitle = 'Next.js Sample Website!!';
+const Layout = ({ children, home }) => {
+    // layout -> 모든페이지에서 공용으로 사용 할 페이지.
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <a>
+                                <Image
+                                    priority
+                                    src="/images/profile.jpg"
+                                    className={utilStyles.borderCircle}
+                                    height={108}
+                                    width={108}
+                                    alt={name}
+                                />
+                            </a>
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>
+                {
+                    children
+                }
+            </main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    )
+};
+
+export default Layout;
---
+++ pages/_app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/global.css'
+const App = ({ Component, pageProps }) => {
+    return (
+        <h1> 안녕 ㅎ2 </h1>
+    )
+}
+
+export default App;
